a4d88fe94622e598b93587ed3f331100d841d1e4
JXKUB <125495284+JXKUB-DEV@users.noreply.github.com>
2026-07-28 14:45:45 +0200

    Test AIC8800D80 driver compilation [skip ci]
    Install necessary packages and clone AIC8800D80 driver repository.

--- build_files/install-aic8800.sh
+++ build_files/install-aic8800.sh
@@ -2,4 +2,20 @@
 
 set -ouex pipefail
 
-echo "Missouri OS: AIC8800D80 driver installation placeholder"
+DRIVER_DIR="/tmp/aic8800d80"
+
+dnf5 install -y \
+  git \
+  gcc \
+  make \
+  elfutils-libelf-devel \
+  kernel-devel \
+  kernel-headers
+
+git clone --depth=1 \
+  https://github.com/shenmintao/aic8800d80.git \
+  "${DRIVER_DIR}"
+
+cd "${DRIVER_DIR}/drivers/aic8800"
+
+make
